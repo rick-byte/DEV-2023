8ca76f34a674301c73ddae279ddd846f7619e3f6
RITIK <meritik11@gmail.com>
2023-06-28 13:21:18 +0530

    First launch

---
+++ birth.js
@@ -0,0 +1,7 @@
+document.getElementById("btn").addEventListener("click", function() {
+    var card = document.getElementById("card");
+    var invitation = document.getElementById("invitation");
+
+    card.style.display = "none";
+    invitation.style.display = "block";
+});
